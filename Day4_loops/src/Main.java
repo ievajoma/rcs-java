f06e52d05c26693850daeef72c94052c835d59ad
ievajoma <162178511+ievajoma@users.noreply.github.com>
2024-03-15 08:32:17 +0200

    Klasesdarbs2

--- Day4_loops/src/Main.java
+++ Day4_loops/src/Main.java
@@ -2,7 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        task2();
+    }
 
+    public static void task2(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ievadiet skaitli!");
+        String simbol = sc.nextLine();
+        int skaitlis = Integer.parseInt(simbol);
+
+        if(skaitlis >= 10 || skaitlis <= 5){
+            System.out.println("$$");
+        }else if(skaitlis >= 6 && skaitlis <= 9){
+            System.out.println("$");
+        }
+
+    }
+
+    public static void task1(){
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Ievadiet simbolu virkni!");
