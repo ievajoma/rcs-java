7d760ab95b0e5f1be89936313f66fd3d5a50383b
ievajoma <162178511+ievajoma@users.noreply.github.com>
2024-03-22 20:08:07 +0200

    Klasesdarbs2 5diena

--- Day5_Loops/src/Main.java
+++ Day5_Loops/src/Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
+//        task1();
+        task2();
     }
 
     public static void task1() {
@@ -18,5 +19,18 @@ public class Main {
             result = result * i;
         }
         System.out.println("Faktoriālis ir " + result);
+    }
+
+    public static void task2() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ievadiet skaitli");
+        int skaitlis = sc.nextInt();
+        System.out.println("Ievadiet skaitli - kāpinātāju");
+        int kapinatajs = sc.nextInt();
+        int result = skaitlis;
+        for(int i = 1; i <= kapinatajs; i++){
+            result = result * skaitlis;
+        }
+        System.out.println("Kāpinājums skaitlim " + skaitlis + " ir " + result);
     }
 }
