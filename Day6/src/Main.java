a732848971337b010be9aef5728f90368d9d5d21
ievajoma <162178511+ievajoma@users.noreply.github.com>
2024-03-29 19:44:20 +0200

    6.dienas klasesdarbs1

---
+++ Day6/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        sample1("jgfkla", "kj", "lkjagf");
+    }
+
+    public static void sample1(String a, String b, String c){
+        String visi = a + b + c;
+        System.out.println(visi);
+    }
+}
