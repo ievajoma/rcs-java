9eec69916eaa77ab413d6ad115955a447f085e76
ievajoma <162178511+ievajoma@users.noreply.github.com>
2024-03-29 20:57:38 +0200

    6.dienas klasesdarbs2

--- Day6/src/Main.java
+++ Day6/src/Main.java
@@ -3,10 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         sample1("jgfkla", "kj", "lkjagf");
+        task2(3);
     }
 
     public static void sample1(String a, String b, String c){
         String visi = a + b + c;
         System.out.println(visi);
+    }
+
+    public static void task2(int a){
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+        for(int i = 0; i < a; i++ ){
+            System.out.println("Ievadiet skaitli!");
+            int x = sc.nextInt();
+            result = result + x;
+        }
+        System.out.println(result);
     }
 }
