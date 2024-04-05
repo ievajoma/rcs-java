33cc4f0662e2bb26edb84bc602490bd3ab12fc4f
ievajoma <162178511+ievajoma@users.noreply.github.com>
2024-04-05 21:45:29 +0300

    7_dienas_kopsavilkums

--- Day7_methods2/src/main/java/org/example/Main.java
+++ Day7_methods2/src/main/java/org/example/Main.java
@@ -4,7 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task1(7);
+//        task1(7);
+        int result = sample1();
+        System.out.println(result);
+    }
+
+    public static int sample1(){
+        Scanner sc = new Scanner(System.in);
+        int a = 0;
+        while(a < 1){
+                System.out.println("Ievadiet pozitīvu skaitli!");
+                a = sc.nextInt();
+        }return a;
     }
 
     public static void task1(int times) {
@@ -12,12 +23,13 @@ public class Main {
         int sum = 0;
 
         if(times % 2 ==0){
-            for(int i = 0; i < times; i++){
+            for(int i = 0; i < times; i++) {
                 System.out.println("Ievadiet skaitli!");
                 int number = sc.nextInt();
                 sum = sum + number;
-        } else
-            for (int i = 0; (i < sum) < 15; i++) {
+            }
+        }else{
+            while(sum < 15){
                 System.out.println("Ievadiet skaitli!");
                 int number = sc.nextInt();
                 sum = sum + number;
